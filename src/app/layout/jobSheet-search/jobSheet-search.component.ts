9688ac9e21705c9a4f9d709991b0b4224ba93256
Md. Zeeshan HAIDER <mdzeeshan.haider.external@axa.be>
2018-12-12 16:50:11 +0530

    Job Sheet Search UI completed

--- src/app/layout/jobSheet-search/jobSheet-search.component.ts
+++ src/app/layout/jobSheet-search/jobSheet-search.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { routerTransition } from '../../router.animations';
 
 @Component({
     selector: 'jobSheet-search',
     templateUrl: './jobSheet-search.component.html',
-    styleUrls: ['./jobSheet-search.component.scss']
+    styleUrls: ['./jobSheet-search.component.scss'],
+    animations: [routerTransition()]
 })
 export class JobSheetSearchComponent implements OnInit {
     constructor() {}
