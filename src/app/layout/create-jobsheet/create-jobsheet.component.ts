e35358cce0346a2fca279970ff47366e0aaed110
Md. Zeeshan HAIDER <mdzeeshan.haider.external@axa.be>
2018-12-19 20:19:51 +0530

    Changes in Create Jobsheet page

--- src/app/layout/create-jobsheet/create-jobsheet.component.ts
+++ src/app/layout/create-jobsheet/create-jobsheet.component.ts
@@ -1,11 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { routerTransition } from '../../router.animations';
 
 @Component({
     selector: 'create-jobsheet',
     templateUrl: './create-jobsheet.component.html',
-    styleUrls: ['./create-jobsheet.component.scss']
+    styleUrls: ['./create-jobsheet.component.scss'],
+    animations: [routerTransition()]
 })
 export class CreateJobsheetComponent implements OnInit {
+    public isCollapsedPanel1 = false;
+    public isCollapsedPanel2 = false;
     constructor() {}
 
     ngOnInit() {}
--- src/app/layout/create-jobsheet/create-jobsheet.module.ts
+++ src/app/layout/create-jobsheet/create-jobsheet.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { CreateJobsheetRoutingModule } from './create-jobsheet-routing.module';
 import { CreateJobsheetComponent } from './create-jobsheet.component';
 
 @NgModule({
-    imports: [CommonModule, CreateJobsheetRoutingModule],
+    imports: [CommonModule, CreateJobsheetRoutingModule, NgbModule],
     declarations: [CreateJobsheetComponent]
 })
 export class CreateJobsheetModule {}
